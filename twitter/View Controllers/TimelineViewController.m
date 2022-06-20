13ca6c645dcec0f94414069f59502ef8217a8796
Harini Sundaram <harinisundaram@fb.com>
2022-06-20 15:57:46 -0700

    Created Logout Functionality

--- twitter/View Controllers/TimelineViewController.m
+++ twitter/View Controllers/TimelineViewController.m
@@ -8,12 +8,24 @@
 
 #import "TimelineViewController.h"
 #import "APIManager.h"
+#import "AppDelegate.h"
+#import "LoginViewController.h"
 
 @interface TimelineViewController ()
+- (IBAction)didTapLogout:(id)sender;
 
 @end
 
 @implementation TimelineViewController
+- (IBAction)didTapLogout:(id)sender {
+    // TimelineViewController.m
+    AppDelegate *appDelegate = (AppDelegate *)[UIApplication sharedApplication].delegate;
+
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    LoginViewController *loginViewController = [storyboard instantiateViewControllerWithIdentifier:@"LoginViewController"];
+    appDelegate.window.rootViewController = loginViewController;
+    [[APIManager shared] logout];
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
